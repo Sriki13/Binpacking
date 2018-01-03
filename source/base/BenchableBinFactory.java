c5707568db9e2861088f84ca70eff5f5ca9190bb
JA <jean-adam.puskaric@etu.unice.fr>
2018-01-03 21:59:43 +0100

    "JA tu peux push le fix de -bench stp"

--- source/base/BenchableBinFactory.java
+++ source/base/BenchableBinFactory.java
@@ -4,7 +4,9 @@ public class BenchableBinFactory extends BinFactory {
 
     @Override
     public Bin createBin(int binSize) {
-        return new BenchableBin(new ConcreteBin(binSize,index++));
+        Bin bin = new BenchableBin(new ConcreteBin(binSize, index++));
+        created.add(bin);
+        return bin;
     }
 
 }
